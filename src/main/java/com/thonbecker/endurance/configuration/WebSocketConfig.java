182d7179c1548d153549c66e3b2b5d86eb3271cf
Thon Becker <me@thonbecker.com>
2024-03-29 14:17:12 +0000

    chore(app): format fix

--- src/main/java/com/thonbecker/endurance/configuration/WebSocketConfig.java
+++ src/main/java/com/thonbecker/endurance/configuration/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.thonbecker.endurance.configuration;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
