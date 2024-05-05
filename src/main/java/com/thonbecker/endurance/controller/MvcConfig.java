e72d9700826524dca4c60d31fe7f21ebcff2907d
Thon Becker <me@thonbecker.com>
2024-05-05 12:38:12 +0000

    feat(app): adjust images

--- src/main/java/com/thonbecker/endurance/controller/MvcConfig.java
+++ src/main/java/com/thonbecker/endurance/controller/MvcConfig.java
@@ -12,7 +12,6 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(@NonNull ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
 }
