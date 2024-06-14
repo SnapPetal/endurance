3d6f8198bf50a69fd4d86feacef004fcd9330cb3
Thon Becker <thon.becker@daveramsey.com>
2024-06-13 19:14:52 -0500

    chore(app): add spring session jpa

--- src/main/java/com/thonbecker/endurance/configuration/SecurityConfiguration.java
+++ src/main/java/com/thonbecker/endurance/configuration/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.thonbecker.endurance.configuration;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.ALWAYS;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 
 @Configuration
 public class SecurityConfiguration {
@@ -17,7 +20,9 @@ public class SecurityConfiguration {
                         .anyRequest()
                         .authenticated())
                 .oauth2Login(Customizer.withDefaults())
-                .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutSuccessUrl("/"));
+                .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutSuccessUrl("/"))
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(ALWAYS))
+                .requestCache(requestCache -> requestCache.requestCache(new HttpSessionRequestCache()));
         return http.build();
     }
 }
---
+++ src/main/java/com/thonbecker/endurance/configuration/SessionConfiguration.java
@@ -0,0 +1,44 @@
+package com.thonbecker.endurance.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+@EnableJdbcHttpSession
+public class SessionConfiguration implements BeanClassLoaderAware {
+
+    private ClassLoader loader;
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("JSESSIONID");
+        serializer.setCookiePath("/");
+        serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$");
+        return serializer;
+    }
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer(objectMapper());
+    }
+
+    private ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModules(SecurityJackson2Modules.getModules(this.loader));
+        return mapper;
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.loader = classLoader;
+    }
+}
