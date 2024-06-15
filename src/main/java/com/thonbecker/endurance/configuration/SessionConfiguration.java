ae22dda7e08963517610b3bd524b30749302161f
Thon Becker <thon.becker@daveramsey.com>
2024-06-14 19:22:54 -0500

    chore(app): add hazelcast

--- src/main/java/com/thonbecker/endurance/configuration/SessionConfiguration.java
+++ src/main/java/com/thonbecker/endurance/configuration/SessionConfiguration.java
@@ -1,14 +1,38 @@
 package com.thonbecker.endurance.configuration;
 
+import com.hazelcast.config.*;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.session.MapSession;
+import org.springframework.session.hazelcast.HazelcastIndexedSessionRepository;
+import org.springframework.session.hazelcast.HazelcastSessionSerializer;
+import org.springframework.session.hazelcast.PrincipalNameExtractor;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
-@EnableJdbcHttpSession
+@EnableHazelcastHttpSession
 public class SessionConfiguration {
+
+    @Bean
+    public HazelcastInstance hazelcastInstance() {
+        Config config = new Config();
+        AttributeConfig attributeConfig = new AttributeConfig()
+                .setName(HazelcastIndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
+                .setExtractorClassName(PrincipalNameExtractor.class.getName());
+        config.getMapConfig(HazelcastIndexedSessionRepository.DEFAULT_SESSION_MAP_NAME)
+                .addAttributeConfig(attributeConfig)
+                .addIndexConfig(
+                        new IndexConfig(IndexType.HASH, HazelcastIndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE));
+        SerializerConfig serializerConfig = new SerializerConfig();
+        serializerConfig.setImplementation(new HazelcastSessionSerializer()).setTypeClass(MapSession.class);
+        config.getSerializationConfig().addSerializerConfig(serializerConfig);
+        return Hazelcast.newHazelcastInstance(config);
+    }
+
     @Bean
     public CookieSerializer cookieSerializer() {
         DefaultCookieSerializer serializer = new DefaultCookieSerializer();
